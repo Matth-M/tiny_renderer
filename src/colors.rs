caa5eb1fb200bc562bf48d00612110a58e6e8c75
Matth-M <matthieu.matsima@gmail.com>
2024-04-14 18:44:53 +0200

    Idiomatic code

--- src/colors.rs
+++ src/colors.rs
@@ -8,26 +8,26 @@ pub struct Color {
 #[allow(dead_code)]
 impl Color {
     pub fn as_u32(&self) -> u32 {
-        return (self.r as u32) << 16 | (self.g as u32) << 8 | self.b as u32;
+        (self.r as u32) << 16 | (self.g as u32) << 8 | self.b as u32
     }
 
     pub fn red() -> Color {
-        return Color { r: 255, g: 0, b: 0 };
+        Color { r: 255, g: 0, b: 0 }
     }
 
     pub fn green() -> Color {
-        return Color { r: 0, g: 255, b: 0 };
+        Color { r: 0, g: 255, b: 0 }
     }
 
     pub fn white() -> Color {
-        return Color {
+        Color {
             r: 255,
             g: 255,
             b: 255,
-        };
+        }
     }
 
     pub fn from_u8_rgb(r: u8, g: u8, b: u8) -> Color {
-        return Color { r, g, b };
+        Color { r, g, b }
     }
 }
